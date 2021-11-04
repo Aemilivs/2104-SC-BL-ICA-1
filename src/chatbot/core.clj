79f2569f8664f93d52dc5897e2fa722a4dbd6596
Vadim Emelin <vemelin@sessionm.com>
2021-11-04 19:04:33 +0100

    Introduce read-data function.
    And remove excess files containing obsolete functions.

--- src/chatbot/core.clj
+++ src/chatbot/core.clj
@@ -1,7 +1,28 @@
 (ns chatbot.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json]))
+  
+  (defn prompt
+    "Introduce CLI"
+    []
+    (println "Hello username!")
+    (flush)
+    (let [input (read)]
+      (when (not= input 'exit)
+        (println "How can I help you? Type 'exit' to finish")
+        (recur))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  (defn read-data
+    "Read data"
+    []
+    (
+      json/read-str
+      (
+        slurp "resources/data-en.json"
+      )
+    )
+  )
+
+  (defn -main
+    [& args]
+    (print (read-data)))
